f4b7cd7cac31c57b53a98e8b6d7ad35e0b18c094
Jhomer Clemente <jhomer.clemente@edu.sait.ca>
2024-01-16 13:34:46 -0800

    test

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+export default function Page() {
+    return (
+        <main>
+            <h1>Shopping List</h1>
+            <p>Jhomer Clemente</p>
+            <p>
+                <Link href={`https://github.com/Jhmrxclmnt/cprg306-assignments`}> https://github.com/Jhmrxclmnt/cprg306-assignments</Link>
+            </p>
+        </main>
+    );
+}
