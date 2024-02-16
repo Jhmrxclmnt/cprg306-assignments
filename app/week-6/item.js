a5a01b05f0a97cda9da0c6d0942be5098f4efb45
Jhomer Clemente <jhomer.clemente@edu.sait.ca>
2024-02-16 14:13:07 -0800

    updated
    updated

--- app/week-6/item.js
+++
@@ -1,9 +0,0 @@
-export default function Item(props){
-    return(
-        <div>
-            
-            <h2 className="text-xl font-bold">{props.name}</h2>
-            <p className="text-sm">Buy {props.quantity} in {props.category}</p>
-        </div>
-    )   
-}
